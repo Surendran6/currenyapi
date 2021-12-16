0bfb628d8f27e4237aaf91eb23a2769c063a22b8
Surendran6 <88974825+Surendran6@users.noreply.github.com>
2021-12-16 11:28:56 +0530

    Add files via upload

---
+++ currency converter/script.js
@@ -0,0 +1,101 @@
+const currencyDropdown = document.getElementById("from_currency");
+var exchangeBtn = document.querySelector("#exchangeBtn");
+const amountInput = document.getElementById("original-currency-amount");
+const exchangeRateInput = document.getElementById("exchange-rate");
+const originalCurrencyInput = document.getElementById("original-currency-unit");
+const message = document.getElementById("message");
+const feedback = document.getElementById("feedback");
+
+let rates = {};
+let response;
+
+function validateBtn() {
+  try {
+    if (amountInput.value && Number.isInteger(Number(amountInput.value))) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function validateExcahngeRate() {
+  try {
+    if (exchangeRateInput.value) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exchangeBtn.addEventListener("click", () => {
+  try {
+    if (validateBtn()) {
+      if (validateExcahngeRate()) {
+        response = null;
+        let selectedCurrency = currencyDropdown.value;
+        let exchangeRate = rates[selectedCurrency];
+        let amount = parseInt(amountInput.value);
+        let newAmount = exchangeRate * amount;
+
+        feedback.innerHTML = `Your <span class="amount">${amount} Dollar${
+          amount > 1 ? "s" : ""
+        }💰</span> will buy you <span class="amount">${newAmount} ${selectedCurrency}${
+          newAmount > 1 ? "s" : ""
+        }</span>`;
+      } else {
+        response = "Pick a currency";
+      }
+    } else {
+      response = "Input a number digit";
+    }
+    if (response) {
+      message.innerHTML = `Warning!: ${response}`;
+    } else {
+      message.innerHTML = "";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+fetch(
+  "https://api.currencyfreaks.com/latest?apikey=87a1e5af87634a3ca539e73e37e3ec9c&format=json"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    for (rate in data.rates) {
+      currencyDropdown.innerHTML += `<option value=${rate}>${rate}</option>`;
+    }
+    rates = data.rates;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+(function () {
+  let oldVal;
+  const originalCurrency = "Dollar💰";
+  originalCurrencyInput.value = `${originalCurrency}`;
+  amountInput.addEventListener("input", function () {
+    console.log(this.value);
+    if (oldVal !== this.value) {
+      oldVal = this.value;
+      const amount = parseInt(amountInput.value);
+      originalCurrencyInput.value =
+        `Dollar${amount > 1 ? "s" : ""}` + "" + "💰";
+    }
+  });
+})();
+
+function changeExchangeRate() {
+  const selectedCurrency = currencyDropdown.value;
+  const exchangeRate = rates[selectedCurrency];
+  exchangeRateInput.value = exchangeRate;
+
+}
